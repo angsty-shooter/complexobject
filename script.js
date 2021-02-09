3c56203212c2256d1da7c514bca47f16b0356a12
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-09 13:36:07 -0700

    heatbox

---
+++ script.js
@@ -0,0 +1,9 @@
+let microwave={
+    name: "microwave",
+    color: "silver",
+    nickname: "heatbox"
+}
+
+let string = `The ${microwave.color} ${microwave.nickname} lives in the kitchen. Some people call it a ${microwave.name}.`
+
+console.log(string)
